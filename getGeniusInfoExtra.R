11b40496cdba6b3ec2e0ba2513a81abdf5819cdd
sastoudt <sstoudt@berkeley.edu>
2020-02-24 19:34:34 -0800

    get og lyrics for remaining

--- getGeniusInfoExtra.R
+++ getGeniusInfoExtra.R
@@ -1,5 +1,5 @@
 ### get new genius
-
+setwd("~/Desktop/kidz-bop-data")
 extraKB = read.csv("data/missing/extraKB.csv",stringsAsFactors = F)
 extraKB2 = read.csv("data/missing/extraKB2.csv",stringsAsFactors = F)
 
@@ -22,6 +22,8 @@ getOGExtra <- lapply(toGetS, function(x) {
   # artist_name
 })
 save(getOGExtra, file = "data/missing/OG_info_extra.RData")
+load("data/missing/OG_info_extra.RData")
+
 
 cbind.data.frame(id = rep(1:length(getOGExtra),times = unlist(lapply(getOGExtra, nrow))), do.call("rbind", getOGExtra)) %>% View() 
 
@@ -39,16 +41,56 @@ rightOne = c(1, 11, 21, 33, 41,  51, 60, 71, 80, 87,
              1063, 1070, 1080, 1089, 1099, 1108, 1117, 1127
              )
 
+toAdd = cbind.data.frame(id = rep(1:length(getOGExtra),times = unlist(lapply(getOGExtra, nrow))), do.call("rbind", getOGExtra))[rightOne,]
+
+toAdd$rowid = 1:nrow(toAdd)
+View(toAdd[,c(ncol(toAdd),1:3)])
+#toAdd = toAdd[-82,] ## double nothing breaks like a heart
+
+load( file = "data/missing/kbLyricsExtra.RData")
+length(kbLyricsExtra)
+kbLyricsExtra2 = kbLyricsExtra[-c(119:120)]
+
+
+christmasEtc <- c(1, 4, 5, 16, 24, 29, 31, 33, 35, 43, 49, 52, 54, 63, 64, 68, 70, 71, 75, 80, 82, 87, 91, 92, 95, 98, 99, 102, 107, 108, 111, 116)
+
+length(kbLyricsExtra2[-christmasEtc])
+
+kbLyricsExtra = kbLyricsExtra2[-christmasEtc]
+save(kbLyricsExtra, file="data/missing/kbLyricsExtraMatch.RData")
+
+toAdd = toAdd[-christmasEtc,]
+
+## update this, then grab og lyrics
+
+## get extra stuff matched up 
+## get year info
+
+## can run differences when new words are ready
+
+toAdd$song_id[grep("Happy Birthday",toAdd$song_name)] = 76302
+toAdd$song_id[grep("Down",toAdd$song_name)] = 2433932
+toAdd$song_id[grep("Cool",toAdd$song_name)] = 273786
+toAdd$song_id[grep("Gotta Feeling",toAdd$song_name)] = 10692
+toAdd$song_id[grep("MICKEY",toAdd$song_name)] = 846161
+toAdd$song_id[grep("Over Your",toAdd$song_name)] = 392149
+toAdd$song_id[grep("Paradise",toAdd$song_name)] = 3444501
+toAdd$song_id[grep("Wild Ones",toAdd$song_name)] = 68266
+
+safe_scrape <- safely(scrape_lyrics_id) ## make sure if we can't find a song's lyrics, it doesn't crash
+ogLyricsExtra <- map(toAdd$song_id, ~ safe_scrape(.x)) ## patience, this takes awhile
+save(ogLyricsExtra, file = "data/missing/ogLyricsExtra.RData")
+
 
 # ditch first one anyway
-# replace birthday with https://genius.com/The-beatles-birthday-lyrics
-# replace down with https://genius.com/Marian-hill-down-lyrics
-# replace cool with https://genius.com/Gwen-stefani-cool-lyrics
-# fix i got a feeling https://genius.com/The-black-eyed-peas-i-gotta-feeling-lyrics
-# fix mickey https://genius.com/Toni-basil-mickey-12-version-lyrics
-# over you https://genius.com/Daughtry-over-you-lyrics
-# paradise https://genius.com/George-ezra-paradise-lyrics
+# replace birthday with https://genius.com/The-beatles-birthday-lyrics 76302
+# replace down with https://genius.com/Marian-hill-down-lyrics 2433932
+# replace cool with https://genius.com/Gwen-stefani-cool-lyrics 273786
+# fix i got a feeling https://genius.com/The-black-eyed-peas-i-gotta-feeling-lyrics 10692
+# fix mickey https://genius.com/Toni-basil-mickey-12-version-lyrics  846161
+# over you https://genius.com/Daughtry-over-you-lyrics 392149
+# paradise https://genius.com/George-ezra-paradise-lyrics 3444501
 # two nothing breaks like a heart?
-# wild ones https://genius.com/Flo-rida-wild-ones-lyrics
+# wild ones https://genius.com/Flo-rida-wild-ones-lyrics 68266
 # it's your birthday, kidz bop shuffle, life of the party, sounds like a hit, is kb og
 ## ugh christmas, pb jelly time

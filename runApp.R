d473ef0c617f27af22ada50abc49c172e0fd97b6
sastoudt <sstoudt@berkeley.edu>
2020-03-05 04:57:30 -0800

    update run app wiht directory example

--- runApp.R
+++ runApp.R
@@ -1,6 +1,6 @@
 install.packages("shiny")
 library(shiny)
-wd = "" ## put path to shiny directory here
+wd = "" ## put path to shiny directory here "~/Desktop/kidz-bop-data"
 setwd(wd)
 
 runApp("shiny")

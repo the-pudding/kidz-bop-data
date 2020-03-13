70457de9ff9ebf442168ccde9e40f26fce540e22
sastoudt <sstoudt@berkeley.edu>
2020-03-12 20:23:47 -0700

    kb-proportions so so close

--- moving_to_final/censorByCategoryYear.R
+++ moving_to_final/censorByCategoryYear.R
@@ -27,5 +27,95 @@ which(badWords$bad_word=="vodka") ## 230
 badWords = badWords[-c(98, 115, 119, 152, 11, 154, 174, 230),]
 
 allC=merge(allC, badWords[,1:2], by.x=c("badword"), by.y="bad_word", all.x=T)
+allC$song_name = tolower(allC$song_name)
+
+datesI=read.csv("data/censoring/fullDataSet.csv",stringsAsFactors = F)
+
+setdiff(unique(allC$song_name), datesI$kb_song_name)
+
+datesM = read.csv("data/missing/extraDates.csv", stringsAsFactors = F)
+datesM$song_name= tolower(datesM$song_name)
+#testM = merge(allC, datesI, by.x="song_name", by.y="kb_song_name",all.x=T)
+
+setdiff(unique(allC$song_name), c(datesI$kb_song_name, tolower(datesM$song_name)))
+
+helper <- function(x){
+  try1=which(datesI$kb_song_name == x)
+  try2=which(datesM$song_name == x)
+  #print(x)
+  if(length(try1)==0){
+    if(length(try2)==0){
+      return(NA)
+    }else{
+      return(datesM[try2[1],"kb_release_date"])
+    }
+  }else{
+    return(datesI[try1[1], "kb_release_year"])
+  }
+ 
+}
+
+tryThis  = lapply(allC$song_name, helper)
+
+allC$year = unlist(tryThis)
+
+eachSong = allC %>% group_by(song_name, og_artist) %>% summarise(year = year[1])
+
+eachSong[which(is.na(eachSong$year)),]
+
+write.csv(eachSong[which(is.na(eachSong$year)),],"moving_to_final/missingYears.csv", row.names=F)
+
+my = read.csv("moving_to_final/missingYears.csv", stringsAsFactors = F)
+
+test = eachSong[which(is.na(eachSong$year)),]
+test$year = my$year
+
+allC2 = merge(allC, test, by.x=c("song_name","og_artist"), by.y=c("song_name","og_artist"),all.x=T)
+
+View(allC2)
+
+allC2$year = ifelse(is.na(allC2$year.x), allC2$year.y, allC2$year.x)
+
+idx = grep("/",allC2$year) 
+allC2$year[idx]=paste0("20",unlist(lapply(strsplit(grep("/",allC2$year, value = T), "/" ), function(x){x[3]}))) #%>% unlist() %>% length()
+
+write.csv(allC2, "moving_to_final/data/proportions-kb-prep.csv", row.names=F)
 
 
+###
+
+all = read.csv("moving_to_final/data/proportions-kb-prep.csv", stringsAsFactors = F)
+
+
+all$isCensored = ifelse(all$numOccurKB < all$numOccurOG, 1, 0)
+all$isPresent = ifelse(all$numOccurOG>0, 1, 0)
+
+require(ggplot2)
+all %>% group_by(year) %>% summarise(prop=sum(isCensored)/n()) %>% ggplot(., aes(year, prop))+geom_point()
+
+
+byYearGroup= all %>% group_by(year,category) %>% summarise(totalCensoredG= sum(isCensored), totalExistG = sum(isPresent)) 
+
+propCensoredByYear = all %>% group_by(year) %>% summarise(propCensored = sum(isCensored)/sum(isPresent), totalCensored = sum(isCensored))
+
+test = merge(byYearGroup, propCensoredByYear, by.x="year", by.y="year")
+test$tryThis = test$totalCensoredG/test$totalCensored * test$propCensored
+
+test2=subset(test, year ==2001)
+
+sum(test2$tryThis)
+
+
+require(tidyr)
+
+spread(test[,c("year","category","tryThis")], category, tryThis) %>% View()
+apply(spread(test[,c("year","category","tryThis")], category, tryThis)[,-1] , 1, sum)
+
+propCensoredByYear$propCensored
+
+tidyV = spread(test[,c("year","category","tryThis")], category, tryThis)
+
+write.csv(tidyV, "moving_to_final/data/proportions-kb-almost.csv", row.names=F)
+
+
+ggplot(test, aes(year, total))+geom_point()
--- moving_to_final/updateDataForCensorSummaries.R
+++ moving_to_final/updateDataForCensorSummaries.R
@@ -94,6 +94,8 @@ ogCensored$id =  rep(1:nrow(crosswalk), nrow(badWords))
 ## need song title and artist
 
 song_name = lapply(rep(crosswalk$kb_idx,nrow(badWords)), function(x){kbLyrics[[x]]$song_name[1]})
+song_name2 = lapply(rep(crosswalk$og_idx,nrow(badWords)), function(x){ogLyricsFull[[x]]$song_name[1]})
+
 og_artist = lapply(rep(crosswalk$og_idx,nrow(badWords)), function(x){ogLyricsFull[[x]]$artist_name[1]})
 
 length(unlist(song_name)) == length(rep(crosswalk$kb_idx,nrow(badWords)))
@@ -104,9 +106,16 @@ length(unlist(lapply(song_name, function(x){ifelse(is.null(x),NA, x)})))== lengt
 length(unlist(lapply(og_artist, function(x){ifelse(is.null(x),NA, x)})))== length(rep(crosswalk$kb_idx,nrow(badWords)))
 
 song_name = unlist(lapply(song_name, function(x){ifelse(is.null(x),NA, x)}))
+song_name2 = unlist(lapply(song_name2, function(x){ifelse(is.null(x),NA, x)}))
+
 og_artist = unlist(lapply(og_artist, function(x){ifelse(is.null(x),NA, x)}))
 
-kbCensored$song_name=song_name
+helperSN = function(x,y){
+  ifelse(is.na(x),y,x)
+}
+song_nameF = mapply(helperSN, song_name, song_name2)
+
+kbCensored$song_name=song_nameF
 kbCensored$og_artist = og_artist
 
 all = merge(kbCensored, ogCensored, by.x=c("id", "badword"), by.y = c("id", "badword"))
@@ -183,6 +192,7 @@ ogCensored2$id =  rep(1:length(ogLyricsExtra), nrow(badWords))
 ## need song title and artist
 
 song_name = lapply(rep(length(kbLyricsExtra),nrow(badWords)), function(x){kbLyricsExtra[[x]]$result$song_name[1]})
+song_name2 = lapply(rep(length(kbLyricsExtra),nrow(badWords)), function(x){ogLyricsExtra[[x]]$result$song_name[1]})
 og_artist = lapply(rep(length(ogLyricsExtra),nrow(badWords)), function(x){ogLyricsExtra[[x]]$result$artist_name[1]})
 
 length(unlist(song_name)) == length(rep(crosswalk$kb_idx,nrow(badWords)))
@@ -193,7 +203,15 @@ length(unlist(lapply(song_name, function(x){ifelse(is.null(x),NA, x)})))== lengt
 length(unlist(lapply(og_artist, function(x){ifelse(is.null(x),NA, x)})))== length(rep(length(ogLyricsExtra),nrow(badWords)))
 
 song_name = unlist(lapply(song_name, function(x){ifelse(is.null(x),NA, x)}))
+song_name2 = unlist(lapply(song_name2, function(x){ifelse(is.null(x),NA, x)}))
+
 og_artist = unlist(lapply(og_artist, function(x){ifelse(is.null(x),NA, x)}))
+
+helperSN = function(x,y){
+  ifelse(is.na(x),y,x)
+}
+song_nameF = mapply(helperSN, song_name, song_name2)
+
 
 kbCensored2$song_name=song_name
 kbCensored2$og_artist = og_artist
@@ -205,3 +223,4 @@ all2 %>% group_by(id) %>% summarise(count=n())
 allF = rbind.data.frame(all, all2)
 
 write.csv(allF[,-1],"moving_to_final/data/allCensor.csv",row.names=F)
+
